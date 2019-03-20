638d9867f3f4dc71b8e483f1d9888806d5ede0f8
ista2000 <mishra.istasis@gmail.com>
2019-03-20 19:34:27 +0530

    Extended friends page to support search and improved UI

--- assets/src/js/components/Friends/index.js
+++ assets/src/js/components/Friends/index.js
@@ -12,7 +12,14 @@ export default class Friends extends React.Component {
             items: [],
             non_friend_items: [],
             userid: null,
+            school: "",
+            classLevel: "",
+            section: "",
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeschool = this.handleChangeschool.bind(this);
+        this.handleChangeclass = this.handleChangeclass.bind(this);
+        this.handleChangesection = this.handleChangesection.bind(this);
     }
     componentDidMount() {
         fetch("/api/friends")
@@ -33,6 +40,43 @@ export default class Friends extends React.Component {
                     })
                 }
             )
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        let q = "/api/search/user?";
+        if(this.state.school)
+            q += "school=" + this.state.school + "&";
+        if(this.state.classLevel)
+            q += "class=" + this.state.classLevel + "&";
+        if(this.state.section)
+            q += "section=" + this.state.section;
+        fetch(q)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.friends,
+                        non_friend_items: result.non_friends,
+                        userid: result.userid
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                }
+            )
+    }
+    handleChangeschool(event) {
+        this.setState({school: event.target.value});
+    }
+    handleChangeclass(event) {
+        this.setState({classLevel: event.target.value});
+    }
+    handleChangesection(event) {
+        this.setState({section: event.target.value});
     }
     handlefollow(id) {
         handlefollow(this.state.userid, id, 'user');
@@ -68,6 +112,34 @@ export default class Friends extends React.Component {
         } else {
             return (
                     <div className="container" id="react-feed">
+                        <div className="searchbar">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    {/*<label for="school">School:</label>*/}
+                                    <input type="text" className="form-control" placeholder="Enter School" value={this.state.school} onChange={this.handleChangeschool} />
+                                </div>
+                                <div className="col-md-12" style={{padding: '0'}}>
+                                    <div className="form-group col-md-6" style={{display: 'inline-block'}}>
+                                        <label htmlFor="class">Class:</label>
+                                        <select className="form-control" value={this.state.classLevel} onChange={this.handleChangeclass}>
+                                            <option value="">All</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group col-md-6" style={{display: 'inline-block'}}>
+                                        <label htmlFor="section">Section:</label>
+                                        <select className="form-control" value={this.state.section} onChange={this.handleChangesection}>
+                                            <option value="">All</option>
+                                            <option value="A">A</option>
+                                            <option value="B">B</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <button type="submit" value="Submit" className="btn btn-primary col-md-12" style={{marginBottom: '20px'}}>Search</button>
+                            </form>
+                        </div>
                         <div className="shadow">
                             <div className="row">
                                 {
